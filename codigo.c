99c3df9cf76723271152d3ee51471cb7a85a3829
Benjamin Garcia <linkdelira@gmail.com>
2024-10-08 16:25:45 -0600

    First commit

---
+++ codigo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char const *argv[])
+{
+    char correctPassword[] = "patito";
+    char password[10];
+
+    gets(password);
+
+    if (strncmp(password, correctPassword, 10) == 0)
+        printf("\Password correcto. Bienvenido!\n");
+    else
+        printf("\nAcceso denegado.\n");
+}
